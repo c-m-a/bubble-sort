c2105e71db7f23e168a3703bef45ff9176ca1d74
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2019-12-12 12:07:07 -0500

    Adding conditional to check if a block was given or emptyness

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-def bubble_sort_by(array = [])
+def bubble_sort_by(array = [], &block)
   length = array.length
+  return 'Not block given' unless block_given?
 
   length.times do |i|
     array.each_with_index do |_, index|
@@ -11,6 +12,8 @@ def bubble_sort_by(array = [])
       right = array[index + 1]
       result = yield left, right
 
+      return 'Empty block? Seriously?' if result.nil?
+
       next if result.negative?
 
       array[index + 1] = left
@@ -18,5 +21,5 @@ def bubble_sort_by(array = [])
     end
   end
 
-  print "#{array}\n"
+  array
 end
